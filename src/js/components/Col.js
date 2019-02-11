54573f0467cd8dcff3a06f2b0374e253ec994446
WebGoal <webgoal@iMac-de-Igor-Marketing.local>
2019-02-11 14:18:20 -0200

    Fix de lint (https://eslint.org/docs/rules/eqeqeq)

--- src/js/components/Col.js
+++ src/js/components/Col.js
@@ -4,10 +4,10 @@ class Col extends React.Component {
 
     css() {
         let css = 'col-md-' + this.props.md.toString();
-        css += (this.props.lg != undefined) ? ' col-lg-' + this.props.lg: ' ';
-        css += (this.props.sm != undefined) ? ' col-sm-' + this.props.sm: ' ';
-        css += (this.props.xs != undefined) ? ' col-xs-' + this.props.xs: ' ';
-        css += (this.props.custom != undefined) ? this.props.custom : '';
+        css += (this.props.lg !== undefined) ? ' col-lg-' + this.props.lg: ' ';
+        css += (this.props.sm !== undefined) ? ' col-sm-' + this.props.sm: ' ';
+        css += (this.props.xs !== undefined) ? ' col-xs-' + this.props.xs: ' ';
+        css += (this.props.custom !== undefined) ? this.props.custom : '';
 
         return css;
     }
